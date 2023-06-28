99e425f17780568331e75ff713b7f20fc778c7a9
Mohd Kasim <kasimplecoder@email.com>
2023-06-28 23:53:28 +0530

    new changes are done

--- lib/screens/main_home_screen.dart
+++ lib/screens/main_home_screen.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../widgets/home_bottomnavigationbar.dart';
+
 import 'indiarupee_screen.dart';
 
 class Home extends StatefulWidget {
